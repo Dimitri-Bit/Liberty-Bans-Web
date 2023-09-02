843163f95ee9527fc105ce54236a6761d74d289c
Dimitri-Bit <dimitrijefustic0@gmail.com>
2023-09-02 23:14:12 +0200

    Added UUID search option

--- src/main/java/me/dimitri/libertyweb/service/SearchService.java
+++ src/main/java/me/dimitri/libertyweb/service/SearchService.java
@@ -1,6 +1,7 @@
 package me.dimitri.libertyweb.service;
 
 import jakarta.inject.Singleton;
+import me.dimitri.libertyweb.api.UsernameAPI;
 import me.dimitri.libertyweb.model.response.WebSearchResponse;
 import me.dimitri.libertyweb.repository.LookupRepository;
 import me.dimitri.libertyweb.repository.SearchRepository;
@@ -19,8 +20,35 @@ public class SearchService {
         this.searchRepository = searchRepository;
     }
 
-    public WebSearchResponse searchUser(String username) {
-        Optional<UUID> userUUID = lookupRepository.lookupUUID(username);
-        return userUUID.map(uuid -> searchRepository.query(uuid, username)).orElse(null);
+//    public WebSearchResponse searchUser(String username) {
+//
+//
+//
+//        Optional<UUID> userUUID = lookupRepository.lookupUUID(username);
+//        return userUUID.map(uuid -> searchRepository.query(uuid, username)).orElse(null);
+//    }
+
+    public WebSearchResponse searchUser(String input) {
+        Optional<UUID> userUUID = convertUUID(input);
+        Optional<String> username;
+
+        if (userUUID.isPresent()) { // Search used UUID
+            username = lookupRepository.lookupUsername(userUUID.get());
+        } else {
+            username = Optional.of(input);
+            userUUID = lookupRepository.lookupUUID(username.get());
+        }
+
+        if (userUUID.isPresent() && username.isPresent()) {
+            return searchRepository.query(userUUID.get(), username.get());
+        }
+        return null;
+    }
+
+    private Optional<UUID> convertUUID(String input) {
+        try {
+            return Optional.of(UUID.fromString(input));
+        } catch (Exception ignored) {}
+        return Optional.empty();
     }
 }
