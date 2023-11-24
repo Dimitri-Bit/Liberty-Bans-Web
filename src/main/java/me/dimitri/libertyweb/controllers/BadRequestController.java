6c52de7fa399042cf303a59d3cf2a32a719972ca
dimitri <dimitrifustic@gmail.com>
2023-11-24 19:29:00 +0100

    Added 404 & 400 error routes

--- src/main/java/me/dimitri/libertyweb/controllers/BadRequestController.java
+++ src/main/java/me/dimitri/libertyweb/controllers/BadRequestController.java
@@ -1,4 +1,20 @@
 package me.dimitri.libertyweb.controllers;
 
-public class ErrorController {
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Controller("/badRequest")
+public class BadRequestController {
+
+    @Error(status = HttpStatus.BAD_REQUEST, global = true)
+    public HttpResponse<?> notFound(HttpRequest request) throws URISyntaxException {
+        URI location = new URI("/400.html");
+        return HttpResponse.redirect(location);
+    }
 }
--- src/main/java/me/dimitri/libertyweb/controllers/NotFoundController.java
+++ src/main/java/me/dimitri/libertyweb/controllers/NotFoundController.java
@@ -1,4 +1,22 @@
 package me.dimitri.libertyweb.controllers;
 
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.HttpStatus;
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Error;
+import io.micronaut.http.hateoas.JsonError;
+import io.micronaut.http.hateoas.Link;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Controller("/notfound")
 public class NotFoundController {
+
+    @Error(status = HttpStatus.NOT_FOUND, global = true)
+    public HttpResponse<?> notFound(HttpRequest request) throws URISyntaxException {
+        URI location = new URI("/404.html");
+        return HttpResponse.redirect(location);
+    }
 }
