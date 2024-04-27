80b3b68d82cdc401858dc3db58ddf4e6ff987c09
KoxSosen <67807644+KoxSosen@users.noreply.github.com>
2024-04-27 20:17:34 +0200

    Final fixes.

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -162,18 +162,18 @@ $(document).ready(function () {
         }
     }
 
-    function setRowData(victimUUID, victimUsername, operatorUUID, operatorUsername, reason, row, startDate, expirationDate, timeUntilExpirationDate, length, label) {
+    function setRowData(victimUUID, victimUsername, operatorUUID, operatorUsername, reason, startDate, expirationDate, timeUntilExpirationDate, length, label, row) {
         $(`#first-uuid-${row}`).attr('src', `https://visage.surgeplay.com/face/55/${victimUUID}`);
         $(`#offender-${row}`).text(`${victimUsername}`);
         $(`#second-uuid-${row}`).attr('src', `https://visage.surgeplay.com/face/55/${operatorUUID}`);
         $(`#operator-${row}`).text(`${operatorUsername}`);
         $(`#reason-${row}`).text(`${reason}`);
-        $(`#status-badge-${row}`).text(`${label}`);
 
         $(`#punishment-start-date-${row}`).text(`${startDate}`);
         $(`#punishment-end-date-${row}`).text(`${expirationDate}`);
-        $(`#punishment-length-${row}`).text(`${timeUntilExpirationDate}`);
-        $(`#punishment-expire-date-${row}`).text(`${length}`);
+        $(`#punishment-length-${row}`).text(`${length}`);
+        $(`#punishment-expire-date-${row}`).text(`${timeUntilExpirationDate}`);
+        $(`#status-badge-${row}`).text(`${label}`);
     }
 
     function setMorePages(morePages) {
@@ -239,7 +239,7 @@ $(document).ready(function () {
         // We actually got data!
         getWithAsyncFetch(`/punishments/${type}/${page}`).then(data => {
             let rowCount = 0;
-            for (const key in data.punishments) {
+            for (let key in data.punishments) {
                 setPunishmentStyle(data.punishments[key].label, rowCount);
 
                 let operatorUUID = data.punishments[key].operatorUuid;
@@ -258,7 +258,7 @@ $(document).ready(function () {
                     timeUntilExpiration = calculateExpirationDate(expirationDate);
                 }
 
-                setRowData(data.punishments[key].victimUuid, data.punishments[key].victimUsername, operatorUUID, data.punishments[key].operatorUsername, data.punishments[key].reason, rowCount, data.punishments[key].startDate, data.punishments[key].endDate, timeUntilExpiration, data.punishments[key].punishmentLength, data.punishments[key].label);
+                setRowData(data.punishments[key].victimUuid, data.punishments[key].victimUsername, operatorUUID, data.punishments[key].operatorUsername, data.punishments[key].reason, data.punishments[key].startDate, data.punishments[key].endDate, timeUntilExpiration, data.punishments[key].punishmentLength, data.punishments[key].label, rowCount);
                 rowCount++;
             }
 
